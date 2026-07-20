8d41c2b9b0b16e8cf2a6fa2d5276c89e747e9b30
zyc771 <2399287027@qq.com>
2026-07-20 15:28:44 +0800

    新功能：乘方

--- calc.py
+++ calc.py
@@ -12,7 +12,11 @@ def divide(a, b):
         return "错误：不能除以0"
     return a / b
 
+def power(a, b):
+    return a**b
+
 print(f"1 + 2 = {add(1, 2)}")
 print(f"5 - 3 = {subtract(5, 3)}")
 print(f"3 × 4 = {multiply(3, 4)}")
 print(f"5 / 0 = {divide(5, 0)}")
+print(f"2^3 = {power(2, 3)}")
